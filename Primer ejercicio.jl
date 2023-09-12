d7d70e214ff8a6ee2a515e4fa286da79c7662384
2stephen0 <144471954+2stephen0@users.noreply.github.com>
2023-09-11 22:20:15 -0500

    Add files via upload

---
+++ Primer ejercicio.jl
@@ -0,0 +1,16 @@
+#suma de los elementos de las filas impares para una matriz de números aleatorios de dimensión 100x100
+a=rand(100,100)
+cero=0
+#bucle
+for j=1:100
+    if j in 2j+j
+    
+        suma_impar=sum(a[j])
+      cero =suma_impar
+    end
+end
+println("La suma de las filas impares es " , cero)
+
+
+
+
