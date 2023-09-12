8343c44fd246b82fa9a12f51af26ed5d952fd8f9
2stephen0 <estebanandres924@gmail.com>
2023-09-11 23:04:19 -0500

    Sucesi\303\263n de Fibonacci.jl

--- Sucesión de Fibonacci.jl
+++ Sucesión de Fibonacci.jl
@@ -1,7 +1,7 @@
 #Mostrar series de Fibonacci hasta 10 términos.
 
-b = 1:1:10
-n = 10
+b = 1:1:100
+n = 100
 y = zeros(length(b)) 
 
 y[1] = 0  
@@ -11,7 +11,12 @@ for i in 3:n
     y[i] = y[i-1] + y[i-2]
 end
 
-println(y)
+   a=println(y);
+
+
+   using Plots
+
+plot(b,y)
 
 
 
