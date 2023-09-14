35522abbf66f636d02d5da3bc30186e6ebd2518f
2stephen0 <estebanandres924@gmail.com>
2023-09-13 20:37:47 -0500

    M\303\251todo de newton.jl

---
+++ Método de newton.jl
@@ -0,0 +1,31 @@
+using Plots
+using Pkg
+using Calculus
+
+
+f(x) = x^3-2x-5 ; n=1
+dx=derivative(f, 2)
+function newton_rapson(f,x,dx,tol,inter)
+   
+   x+n=x-(f(x)/a)  ; g(x)=x+n
+    for i in 1:inter
+
+           b=x 
+
+         if  abs(g(x)-(b)) > tol 
+        
+            g(x)=x-(f(x)/a)
+
+            println("La aproximación más precisa es ", g(x))
+            
+               break
+         else
+
+        end
+  end
+end
+
+newton_rapson(f,2,dx,1*10^(-5),100)
+
+
+
