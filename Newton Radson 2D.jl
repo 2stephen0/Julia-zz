4858d73269ceef1c9785d9a8bea11f39fd497d62
2stephen0 <estebanandres924@gmail.com>
2023-09-27 19:41:38 -0500

    Julia-zz/Newton Radson 2D.jl

--- Newton Radson 2D.jl
+++ Newton Radson 2D.jl
@@ -10,13 +10,13 @@ J(x, y) = [1 3(y^2); 3(x^2) 1]
 
 function newton_rapson(f, x0, y0, tol)
 
-    if f(x0, y0) == [0, 0]
+    if f(x0, y0) == [0; 0]
         i = 0
         i = i + 1
         println("Hay una raiz, la cual esta en el punto ", (x0, y0))
 
     else
-        (x1, y1) = [x0, y0] - (inv(J(x0, y0)) * f(x0, y0))
+        (x1, y1) = [x0; y0] - (inv(J(x0, y0)) * f(x0, y0))
         i = 0
         i = i + 1
 
@@ -24,7 +24,7 @@ function newton_rapson(f, x0, y0, tol)
     while abs(f1(x0, y0)) > tol || abs(f2(x0, y0)) > tol
         i = i + 1
         (x0, y0) = (x1, y1)
-        (x1, y1) = [x0, y0] - (inv(J(x0, y0)) * f(x0, y0))
+        (x1, y1) = [x0; y0] - (inv(J(x0, y0)) * f(x0, y0))
 
     end
     println("la aproximación más precisa está en el punto ", (x1, y1), " y las interacciones necesarias fueron ", i)
